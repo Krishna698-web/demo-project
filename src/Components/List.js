c262d6556d954824292e6b4eeba89c2927686845
Krishan <kmkrider1999@gmail.com>
2023-02-18 22:51:00 +0530

    changes to list component

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ListItem from "./ListItem";
 import Card from "./UI/Card";
+import styles from './List.module.css';
 
 const List = (props) => {
     return (
-        <Card>
+        <Card className={styles.list}>
             <ul>
                 {props.items.map((item) => (
                     <ListItem name={item.name} age={item.age} />
